c6b8306520fe1aeafbea13c11d2397d5822e885e
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-09 16:06:49 -0700

    update

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@ output "alb_hostname" {
   value = aws_alb.main.dns_name
 }
 
-output "name_servers" {
-  description = "DNS Name servers used for the zone"
-  value       = aws_route53_zone.domain_name.name_servers
-}
+# output "name_servers" {
+#   description = "DNS Name servers used for the zone"
+#   value       = aws_route53_zone.domain_name.name_servers
+# }
 
-output "hosted_zone_id" {
-  description = "The hosted zone ID"
-  value       = aws_route53_zone.domain_name.zone_id
-}
+# output "hosted_zone_id" {
+#   description = "The hosted zone ID"
+#   value       = aws_route53_zone.domain_name.zone_id
+# }
