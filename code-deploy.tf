e6166b84cb4c433c8842a5e07b43342a4d26782c
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-10 10:28:37 -0700

    update

--- code-deploy.tf
+++ code-deploy.tf
@@ -33,7 +33,7 @@ resource "aws_codedeploy_deployment_group" "this" {
   load_balancer_info {
     target_group_pair_info {
       prod_traffic_route {
-        listener_arns = ["${aws_lb_listener.front_end.arn}"]
+        listener_arns = ["${aws_alb_listener.front_end.arn}"]
       }
 
       target_group {
