517906ba0587abed7e405d1c8d24278657cd3b85
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-10 11:25:23 -0700

    updage

--- codepipeline.tf
+++ codepipeline.tf
@@ -112,7 +112,6 @@ resource "aws_codepipeline" "cicd_pipeline" {
       output_artifacts = ["code"]
       configuration = {
         FullRepositoryId     = var.git_repo
-        # GitActions will Run the pipeline
         BranchName           = "dev"
         ConnectionArn        = jsondecode(data.aws_secretsmanager_secret_version.codestar_creds.secret_string)["CODESTAR_CREDS"]
         OutputArtifactFormat = "CODE_ZIP"
