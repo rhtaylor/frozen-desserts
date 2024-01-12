c06ed5b39594a65b975a6d0dee107667efc3d0e4
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-12 12:28:32 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
   config.hosts << /.*\.amazonaws\.com/
-
+  config.hosts << /[a-z0-9]+\.amazonaws\.com/
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
