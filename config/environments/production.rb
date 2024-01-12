9fc7ca729bfcfe4af6aa05fd8fcba2b414cf5c96
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-11 21:46:39 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts.clear
+  
+  config.hosts << "load-balancer-125822229.us-east-2.elb.amazonaws.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
