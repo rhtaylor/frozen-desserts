d6fd540fc60ac64438041727332557341288ca76
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-12 17:37:24 -0700

    update

--- alb.tf
+++ alb.tf
@@ -68,14 +68,3 @@ resource "aws_alb_listener" "green" {
   }
 
 }
-
-resource "aws_alb_listener" "rails" {
-  load_balancer_arn = aws_alb.main.id
-  port              = "3000"
-  protocol          = "HTTP"
-
-  default_action {
-    target_group_arn = aws_alb_target_group.rails.id
-    type             = "forward"
-  }
-}
