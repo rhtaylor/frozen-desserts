3f2c3d5de2056b2d1a179d4d60a8ee8512595505
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-13 12:08:37 -0700

    update

--- code-deploy.tf
+++ code-deploy.tf
@@ -6,7 +6,7 @@ resource "aws_codedeploy_app" "this" {
 resource "aws_codedeploy_deployment_group" "this" {
   app_name               = "${aws_codedeploy_app.this.name}"
   deployment_group_name  = "${var.name}-service-deploy-group"
-  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
   service_role_arn       = "${aws_iam_role.codedeploy.arn}"
 
   blue_green_deployment_config {
