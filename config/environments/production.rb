390082897f18922fabb203ef4343eed4b2fa01e3
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-14 17:13:17 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
     "localhost"              # The localhost reserved domain.
   ]
   config.hosts << /.*\.amazonaws\.com/
+  config.hosts << "load-balancer-1444845933.us-east-2.elb.amazonaws.com"
   # config.hosts << /[a-z0-9]+\.amazonaws\.com/
   # config.hosts << "load-balancer-1647984890.us-east-2.elb.amazonaws.com"
   #config.hosts.clear
