03697b2521d8cb5f2d5a0e3224e2e9e3384c2a5e
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-14 17:43:37 -0700

    update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# config/environment.rb
+Rails.application.configure do
+    ...
+    # returns error saying to add "rails_example" to config.hosts
+    config.hosts << /.*\.amazonaws\.com/
+    ...
+    end
