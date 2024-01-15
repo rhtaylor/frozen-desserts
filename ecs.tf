288ad91fc6bf2f308f8e20b431c517b7d05e900f
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-15 10:12:58 -0700

    upate

--- ecs.tf
+++ ecs.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_task_definition" "app" {
     "name": "${var.image_repo_name}",
     "image": "${jsondecode(data.aws_secretsmanager_secret_version.account_id.secret_string)["AWS_ACCOUNT_ID"]}.dkr.ecr.${var.region}.amazonaws.com/${var.image_repo_name}:${var.docker_tag}",
     "essential": true,
-    "networkMode": "awsvpc",
+   
     "entryPoint": [],
      "runtimePlatform": {
         "operatingSystemFamily": "LINUX"
