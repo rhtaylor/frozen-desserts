38a096e8d22d06d1d60a30c14786d98ef743fd6c
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-14 17:45:25 -0700

    update

--- lb.tf
+++ lb.tf
@@ -64,7 +64,7 @@ resource "aws_alb_target_group" "app" {
     healthy_threshold   = "3"
     interval            = "30"
     protocol            = "HTTP"
-    matcher             = "200"
+    matcher             = "200-499"
     timeout             = "3"
     path                = var.health_check_path
     unhealthy_threshold = "2"
@@ -93,7 +93,7 @@ resource "aws_alb_target_group" "app2" {
     healthy_threshold   = "3"
     interval            = "30"
     protocol            = "HTTP"
-    matcher             = "200"
+    matcher             = "200-499"
     timeout             = "3"
     path                = var.health_check_path
     unhealthy_threshold = "2"
