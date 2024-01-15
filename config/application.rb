805ecc359d03d05996de8a9cc390dea4b5afc583
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-14 19:27:20 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module FrozenDesserts
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    Rails.application.config.hosts <<  /.*\.amazonaws\.com/
-    config.hosts << /.*\.amazonaws\.com/
+    application.config.hosts.clear
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
