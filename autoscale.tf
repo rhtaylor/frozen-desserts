6bf350446d4651e7f8fdbd28763dfc884b8b0b1b
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-15 09:06:13 -0700

    upate

--- autoscale.tf
+++ autoscale.tf
@@ -79,7 +79,7 @@ resource "aws_cloudwatch_metric_alarm" "service_cpu_low" {
   namespace           = "AWS/ECS"
   period              = "60"
   statistic           = "Average"
-  threshold           = "10"
+  threshold           = "5"
 
   dimensions = {
     ClusterName = aws_ecs_cluster.main.name
