bc47901b406fb831a341bf0410e5d01b3ec84157
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-20 10:43:19 -0700

    update

--- lb.tf
+++ lb.tf
@@ -88,7 +88,6 @@ resource "aws_alb_target_group" "app2" {
   protocol    = "tcp"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
-
   health_check {
     healthy_threshold   = "3"
     interval            = "30"
@@ -96,8 +95,8 @@ resource "aws_alb_target_group" "app2" {
     matcher             = "200-499"
     timeout             = "3"
     path                = "/" #var.health_check_path
-    unhealthy_threshold = "2"
-  
+    unhealthy_threshold = "2" 
+}
 }
 
 resource "aws_alb_listener" "front_end2" {
