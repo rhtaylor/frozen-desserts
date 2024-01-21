e83d9c5c537467074f7d45b55aa3aed7346b7cca
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-20 18:03:55 -0700

    update

--- lb.tf
+++ lb.tf
@@ -66,7 +66,7 @@ resource "aws_alb_target_group" "app" {
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.main.id
   port              = 80
-  protocol          = "TCP"
+  protocol          = "HTTP"
 
   default_action {
     target_group_arn = aws_alb_target_group.app.id
@@ -80,13 +80,21 @@ resource "aws_alb_target_group" "app2" {
   protocol    = "TCP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
-
+#   health_check {
+#     healthy_threshold   = "3"
+#     interval            = "30"
+#     protocol            = "TCP"
+#     # matcher             = "200-499"
+#     timeout             = "3"
+#     path                = "/" #var.health_check_path
+#     unhealthy_threshold = "2" 
+# }
 }
 
 resource "aws_alb_listener" "front_end2" {
   load_balancer_arn = aws_alb.main.id
   port              = 3000
-  protocol          = "TCP"
+  protocol          = "HTTP"
 
   default_action {
     target_group_arn = aws_alb_target_group.app2.id
