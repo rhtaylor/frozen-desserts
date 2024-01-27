0bd21c3ed751b0185c00f084809495040e310a0d
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-27 16:47:03 -0700

    update

--- railsmigrate.sh
+++ railsmigrate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm -rf /var/lib/dpkg/lock-frontend
+
 which sudo || apt-get install sudo
+sudo rm -rf /var/lib/dpkg/lock-frontend
 apt-get update && apt-get install -y gnupg2
 sudo apt-get install libgemplugin-ruby
 gem install bundler
