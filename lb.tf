0b93c1879a05c7b8c3595f534248d88317d5eeed
rhtaylor <rhtaylorgithub@gmail.com>
2024-01-29 12:17:36 -0700

    update

--- lb.tf
+++ lb.tf
@@ -65,7 +65,7 @@ resource "aws_alb_target_group" "app" {
 
 resource "aws_alb_listener" "front_end" {
   load_balancer_arn = aws_alb.main.id
-  port              = 80
+  port              = 443
   protocol          = "HTTP"
 
   default_action {
